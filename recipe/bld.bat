df65c7a63c7472cc4171bd15e5e6ff2dbffea324
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2024-04-19 15:46:36 +0200

    Split recipe to be able to install logback config alone
    jive and astor use fat-jar files.
    We don't want to depend on jtango (which is already included).
    Create a new output (jtango-logback) that only includes logback configuration files.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,10 +10,3 @@ call mvn install -DskipTests
 REM Generate third party license report
 call mvn license:aggregate-third-party-report
 copy target\site\aggregate-third-party-report.html .
-
-mkdir "%PREFIX%\share\java"
-copy assembly\target\JTango-%PKG_VERSION%.jar %PREFIX%\share\java\JTango.jar
-
-mkdir "%PREFIX%\share\tango"
-copy %RECIPE_DIR%\logback.xml %PREFIX%\share\tango
-copy %RECIPE_DIR%\logback-server.xml %PREFIX%\share\tango
---
+++ recipe/install-logback.bat
@@ -0,0 +1,3 @@
+mkdir "%PREFIX%\share\tango"
+copy %RECIPE_DIR%\logback.xml %PREFIX%\share\tango
+copy %RECIPE_DIR%\logback-server.xml %PREFIX%\share\tango
